75f0b5f3434935e5e62b1d21ec24989283067a92
Daniel van den Berg <daan@daanemanz.nl>
2014-01-18 16:30:22 -0500

    Added specs

--- spec/models/piece_spec.rb
+++ spec/models/piece_spec.rb
@@ -1,6 +1,5 @@
 require 'spec_helper'
 
 describe 'Piece' do
-  it { binding.pry }
   it { should have_many :posts }
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,4 +2,7 @@ require 'spec_helper'
 
 describe 'Post' do
   it { should have_many :pieces }
+
+  it { should respond_to :piece_id }
+  
 end
