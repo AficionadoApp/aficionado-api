87762afbce5b0e0aa593361d4d0ff6217fa45873
Alex Gessner <gessner@shopkeep.com>
2014-01-18 18:05:01 -0500

    push get logic down into model

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -1,36 +1,8 @@
 class PiecesController < ApplicationController
 
   def index
-    museum_json = brooklyn_museum_results(params[:accession_number])
-
-    user_posts = Post.where(accession_number: params[:accession_number]).map(&:to_json)
-
-    hash = museum_json.slice('title', 'uri', 'accession_number')
-    hash['comments'] = user_posts
-    hash['image'] = thumbnail_url(museum_json)
-
-    render json: hash
-  end
-
-private
-
-  def brooklyn_museum_results(accession_number)
-    base_url = 'http://www.brooklynmuseum.org/opencollection/api/'
-    result = RestClient.get( base_url, { params: default_params.merge({ method: 'collection.search', keyword: accession_number }) })
-    JSON.parse(result)["response"]["resultset"]["items"].first
-  end
-
-  def default_params
-    {
-      api_key: ENV['API_TOKEN'],
-      version: 1,
-      item_type: 'object',
-      format: 'json'
-    }
-  end
-
-  def thumbnail_url(full_hash)
-    full_hash.fetch('images', {}).fetch('0', {})['uri']
+    piece = Piece.where(accession_number: params[:accession_number]).first_or_create
+    render json: piece.full_details_hash
   end
 
 end
--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,4 +1,33 @@
 class Piece < ActiveRecord::Base
   has_many :posts
+  BASE_URL = 'http://www.brooklynmuseum.org/opencollection/api/'
 
+  def full_details_hash
+    museum_json = brooklyn_museum_results(self.accession_number)
+
+    hash = museum_json.slice('title', 'uri', 'accession_number')
+    hash['comments'] = posts.map(&:to_json)
+    hash['image'] = thumbnail_url(museum_json)
+    hash
+  end
+
+  private
+
+  def brooklyn_museum_results(accession_number)
+    result = RestClient.get( BASE_URL, { params: default_params.merge({ method: 'collection.search', keyword: accession_number }) })
+    JSON.parse(result)["response"]["resultset"]["items"].first
+  end
+
+  def default_params
+    {
+      api_key: ENV['API_TOKEN'],
+      version: 1,
+      item_type: 'object',
+      format: 'json'
+    }
+  end
+
+  def thumbnail_url(full_hash)
+    full_hash.fetch('images', {}).fetch('0', {})['uri']
+  end
 end
