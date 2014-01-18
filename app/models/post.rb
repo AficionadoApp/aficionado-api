f8c908116cb12b974944dcb4a0e9599f353979a2
Daniel van den Berg <daan@daanemanz.nl>
2014-01-18 16:30:35 -0500

    Added specs

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,9 @@
 class Post < ActiveRecord::Base
   belongs_to :piece
 
+  attr_accessible :accession_number, :name, :comment, :piece_id
 
+  validates :piece_id, presence: true
+  validates :name, presence: true
+  validates :comment, presence: true, length: { maximum: 140 }
 end
