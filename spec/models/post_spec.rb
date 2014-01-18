e45ec5a97c40dcac858b0ecd869b4b8431d1b4ab
Daniel van den Berg <daan@daanemanz.nl>
2014-01-18 17:15:15 -0500

    Finished Post model specs

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,7 +4,9 @@ describe 'Post' do
   before do
     @piece = Piece.new(accession_number: '2002.10-PS-20')
     @piece.save!
+    Timecop.freeze('01/01/1969 00:00:00 UTC')
     @post = Post.new({accession_number: '2002.10-PS-20', name: 'Test User', comment: 'Beautiful painting', piece_id: @piece.id})
+    @post.save!
   end
 
   subject { @post }
@@ -38,6 +40,14 @@ describe 'Post' do
     before { @post.comment = "a" * 141 }
 
     it { should_not be_valid }
+  end
+
+  describe '#to_json' do
+    let(:json) { { name: 'Test User', comment: 'Beautiful painting', time: '1969-01-01 00:00:00 UTC' } }
+
+    it "should create valid json" do
+      @post.to_json.should == json
+    end
   end
   
 end
